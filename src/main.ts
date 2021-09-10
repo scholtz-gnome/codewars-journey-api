1c9922125e49d8c8315b8da1fbac87da88bd470d
Stephen Scholtz <scholtz.gnome@gmail.com>
2021-09-10 17:28:24 +0200

    Use only base url for origin

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: {
-      origin: [
-        'http://localhost:3000',
-        'https://scholtz-gnome.github.io/codewars-journey',
-      ],
+      origin: ['http://localhost:3000', 'https://scholtz-gnome.github.io'],
       credentials: true,
     },
   });
